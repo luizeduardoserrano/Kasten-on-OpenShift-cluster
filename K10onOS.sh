5229d4249dab98e1377f59e5dee7f24d4bb075ba
luiz.serrano <leduardoserrano@gmail.com>
2023-02-15 16:26:47 -0300

    commit

--- K10onOS.sh
+++ K10onOS.sh
@@ -39,4 +39,4 @@ kubectl get secret $sa_secret --namespace kasten-io -ojsonpath="{.data.token}{'\
 echo "port forward” to access the K10 dashboard"
 kubectl --namespace kasten-io port-forward service/gateway 8080:8000
 
-echo "fim"
+echo "end of the script"
