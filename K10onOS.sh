1290a2eeba023474848471ce120af0b4c2810beb
luizeduardoserrano <85516717+luizeduardoserrano@users.noreply.github.com>
2023-02-15 08:27:11 -0300

    Update K10onOS.sh

--- K10onOS.sh
+++ K10onOS.sh
@@ -2,13 +2,4 @@
 echo "list oc nodes"
 oc get nodes
 
-echo "install Helm"
-curl -L https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64 -o /usr/local/bin/helm
-chmod +x /usr/local/bin/helm
-
-# Wait for 120 seconds
-sleep 120
-
-echo "helm version"
-helm version
 
