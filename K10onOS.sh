a77818777cd2889d5ac556019700e7f98622d8b4
luizeduardoserrano <85516717+luizeduardoserrano@users.noreply.github.com>
2023-02-15 08:22:27 -0300

    Update K10onOS.sh

--- K10onOS.sh
+++ K10onOS.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 echo "list oc nodes"
-sudo -i
 oc get nodes
+
+echo "install Helm"
+curl -L https://mirror.openshift.com/pub/openshift-v4/clients/helm/latest/helm-linux-amd64 -o /usr/local/bin/helm
+chmod +x /usr/local/bin/helm
+helm version
+
+echo "update helm repos if already present"
+helm repo update
 
 echo "k10 preflight checks"
 curl https://docs.kasten.io/tools/k10_primer.sh | bash
 
+echo "Creation of the Kasten charts repo and namespace"
+helm repo add kasten https://charts.kasten.io/
+kubectl create namespace kasten-io
 
-helm version
+echo "install Kasten K10"
+helm install k10 kasten/k10 --namespace=kasten-io --set scc.create=true --set route.enabled=true --set route.path="/k10" --set auth.tokenAuth.enabled=true
+
