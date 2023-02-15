9904fde97e9d1d2b45aba3ba3a5a8791bb3ac961
luizeduardoserrano <85516717+luizeduardoserrano@users.noreply.github.com>
2023-02-15 08:25:32 -0300

    Update K10onOS.sh

--- K10onOS.sh
+++ K10onOS.sh
@@ -12,12 +12,3 @@ sleep 120
 echo "helm version"
 helm version
 
-echo "k10 preflight checks"
-curl https://docs.kasten.io/tools/k10_primer.sh | bash
-
-echo "Creation of the Kasten charts repo and namespace"
-helm repo add kasten https://charts.kasten.io/
-kubectl create namespace kasten-io
-
-echo "install Kasten K10"
-helm install k10 kasten/k10 --namespace=kasten-io --set scc.create=true --set route.enabled=true --set route.path="/k10" --set auth.tokenAuth.enabled=true
